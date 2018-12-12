d38a92af628ee7c1357691b80b1b5ccba459d906
NYellapu <nagesh.yellapu@mckesson.eu>
2018-12-13 00:33:32 +0100

    W1U6

--- application/src/main/webapp/controller/App.controller.js
+++ application/src/main/webapp/controller/App.controller.js
@@ -7,7 +7,14 @@ sap.ui.define([
     return Controller.extend("mckesson.eu.my-ui5-app.controller.App", {
 
 		onShowHello : function () {
-			MessageToast.show("Hello openSAP");
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+
 		}
 	});
 
